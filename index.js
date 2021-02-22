1a59679a04e59c02cf4aecdbc5c0edd89ceaa467
zakordonetss <60739399+zakordonetss@users.noreply.github.com>
2021-02-22 16:32:12 +0200

    create error page and trying to add styles

--- index.js
+++ index.js
@@ -3,18 +3,69 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((req, res) => {
-    
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'publick', 'index.html'), (err, data) => {
-            if (err) {
-                throw err
-            }
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            res.end(data)
-        })
+
+    // if (req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+    //         if (err) {
+    //             throw err
+    //         }
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         })
+    //         res.end(data)
+    //     })
+    // } else if (req.url === '/contact') {
+    //     fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+    //         if (err) {
+    //             throw err
+    //         }
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         })
+    //         res.end(data)
+    //     })
+    // }
+
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+    let ext = path.extname(filePath)
+    let contentType = 'text/html'
+
+    switch (ext) {
+        case '.css':
+            contentType = 'text/css'
+            break
+        case '.js':
+            contentType = 'text/javascript'
+            break
+        default:
+            contentType = 'text/html'
     }
+
+    if (!ext) {
+        filePath += '.html'
+    }
+    console.log(filePath);
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+                if (err) {
+                    res.writeHead(500)
+                    res.end('Error')
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type' : 'text/html'
+                    })
+                    res.end(data)
+                }
+            })
+        } else {
+            res.writeHead(200, {
+                'Content-Type' : contentType
+            }) 
+            res.end(content)
+        }
+    })
 
 })
 
